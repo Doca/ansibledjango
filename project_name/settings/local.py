22a492db14971ae56d08c9e84d658c0229e3a991
Dorian Cantzen <cantzen@googlemail.com>
2014-08-01 21:46:29 +0200

    re

--- project_name/settings/local.py
+++ project_name/settings/local.py
@@ -7,7 +7,7 @@ INTERNAL_IPS = ('127.0.0.1',)
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.mysql',
-        'NAME':'testproject',
+        'NAME':'{{ project_name }}',
         'PASSWORD': os.environ['DATABASE_PW'],
         'USER':'root'
     }
